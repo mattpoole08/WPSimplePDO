bb1d9483a774a3cfaf3b3d9ff532f8c39bf4d1ed
Matt Poole <mattpoole09@gmail.com>
2018-03-31 14:24:47 -0400

    added based plugin implementation comments, updated and added gitignore

---
+++ init.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @category     WordPress_Plugin
+ * @package      WPSimplePDO
+ * @author       Matt Poole
+ * @license      GPL-2.0+
+ *
+ * Plugin Name:  WPSimplePDO
+ * Plugin URI:   https://github.com/mattpoole08/WPSimplePDO
+ * Description:  A simple PDO like query structure for WordPress. Uses the built in WordPress Query system by modifying the query string dynamically.
+ * Author:       Matt Poole
+ * Contributors: Matt Poole
+ *
+ * Version:      2.0
+ *
+ *
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * https://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
